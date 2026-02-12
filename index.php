0e20eb3a27cd0cc6e43ed7c33a5c5dc8ca0abf76
Carlo-RetailCare <carlo@retailcare.com.au>
2026-02-12 20:29:22 +1100

    Improve mobile card display and add prominent View Details buttons at top and bottom of cards

--- index.php
+++ index.php
@@ -181,18 +181,30 @@ include __DIR__ . '/includes/header.php'; ?>
                         <h3 class="country-name"><?php echo e($country['country_name']); ?></h3>
                     </div>
                     
-                    <div class="country-badges">
-                        <?php if (!empty($country['is_popular'])): ?>
-                            <span class="popular-badge">
-                                <svg width="14" height="14" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-                                </svg>
-                                <?php echo e(t('popular')); ?>
+                    <div class="country-badges-row">
+                        <div class="country-badges">
+                            <?php if (!empty($country['is_popular'])): ?>
+                                <span class="popular-badge">
+                                    <svg width="14" height="14" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
+                                    </svg>
+                                    <?php echo e(t('popular')); ?>
+                                </span>
+                            <?php endif; ?>
+                            <span class="visa-type-badge <?php echo getVisaTypeBadgeClass($country['visa_type']); ?>">
+                                <?php echo e($visaTypes[$country['visa_type']]); ?>
                             </span>
-                        <?php endif; ?>
-                        <span class="visa-type-badge <?php echo getVisaTypeBadgeClass($country['visa_type']); ?>">
-                            <?php echo e($visaTypes[$country['visa_type']]); ?>
-                        </span>
+                        </div>
+                        <a 
+                            href="country.php?id=<?php echo $country['id']; ?>&lang=<?php echo CURRENT_LANG; ?>" 
+                            class="btn-view-details btn-view-details-top"
+                            aria-label="<?php echo e(t('view_details')); ?> - <?php echo e($country['country_name']); ?>"
+                        >
+                            <?php echo e(t('view_details')); ?>
+                            <svg width="18" height="18" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
+                            </svg>
+                        </a>
                     </div>
                     
                     <div class="country-info-box">
@@ -289,11 +301,11 @@ include __DIR__ . '/includes/header.php'; ?>
                         </div>
                         <a 
                             href="country.php?id=<?php echo $country['id']; ?>&lang=<?php echo CURRENT_LANG; ?>" 
-                            class="btn btn-primary btn-sm btn-full"
+                            class="btn-view-details btn-view-details-bottom"
                             aria-label="<?php echo e(t('view_details')); ?> - <?php echo e($country['country_name']); ?>"
                         >
                             <?php echo e(t('view_details')); ?>
-                            <svg width="16" height="16" viewBox="0 0 20 20" fill="currentColor">
+                            <svg width="18" height="18" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
                             </svg>
                         </a>
